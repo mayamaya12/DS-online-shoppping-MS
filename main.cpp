b2ba7c40183d9d0ebad62f8ec1d48c34bc3ea6fa
mayamaya12 <maya.mohamed@gmail.com>
2026-04-03 04:54:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    MainWindow window;
+    window.setWindowTitle("Spare Parts Management System");
+    window.resize(1200, 800);
+    window.show();
+    window.raise();
+    window.activateWindow();
+    return app.exec();
+}
 
